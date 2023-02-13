846bf2c098d7798059f25d3dccc22c5eff9aff91
Nerovelic <zack.vadhir2@hotmail.com>
2023-02-12 23:28:56 -0800

    integracion del boton y su acciones.

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+// ignore: camel_case_types
 class dice extends StatefulWidget {
   const dice({Key? key}) : super(key: key);
   @override
+  // ignore: library_private_types_in_public_api
   _diceState createState() => _diceState();
 }
 
+// ignore: camel_case_types
 class _diceState extends State<dice> {
+  // ignore: non_constant_identifier_names
   int left_dice = 1;
+  // ignore: non_constant_identifier_names
   int right_dice = 1;
+  void diceOne() {
+    setState(() {
+      left_dice = Random().nextInt(6) + 1;
+      right_dice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,21 +29,18 @@ class _diceState extends State<dice> {
       children: [
         Row(children: <Widget>[
           Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      left_dice = Random().nextInt(6) + 1;
-                    });
-                  },
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: Image.asset('assets/images/dice$left_dice.png'))),
           Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      right_dice = Random().nextInt(6) + 1;
-                    });
-                  },
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: Image.asset('assets/images/dice$right_dice.png'))),
+          ElevatedButton(
+              onPressed: () {
+                diceOne();
+              },
+              child: const Text("Lanza el dado")),
         ])
       ],
     );
